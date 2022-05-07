013d162b4e63edba1c4fdc3499e737c5105c3c03
Peter Sushkin <sushkin.p@gmail.com>
2022-05-07 03:00:24 +0300

    Created 2 new models, made migrations

--- lms/settings.py
+++ lms/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'teachers.apps.TeachersConfig',
+    'groups.apps.GroupsConfig'
 ]
 
 MIDDLEWARE = [
--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    discipline = models.TextField()
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.age} {self.discipline}'
