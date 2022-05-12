9cd0ac668c32d2c28c3dffa5c423665304868043
Peter Sushkin <sushkin.p@gmail.com>
2022-05-12 03:50:06 +0300

    Completed hw_1 (optional task#5)

---
+++ teachers/management/commands/generate_teachers.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from teachers.models import Teacher
+
+
+class Command(BaseCommand):
+    help = 'Generates random teachers and adds them into database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('cnt', type=int, nargs='?', const=1, default=10, help='Amount of teachers to generate')
+
+    def handle(self, *args, **options):
+        cnt = options['cnt']
+
+        if cnt <= 100:
+            Teacher.gen_teachers(cnt)
+            self.stdout.write(self.style.SUCCESS(f'{cnt} teacher(s) created!'))
+        else:
+            raise CommandError('generator is capped at max 100 teachers per use')
+
