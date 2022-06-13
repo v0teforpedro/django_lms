a51213933fda6e461e98208ca0a18d4ad2dcc9b1
Peter Sushkin <sushkin.p@gmail.com>
2022-06-14 01:04:55 +0300

    completed hw_11:
    - added flat pages for 'Contacts'
    - installed CKeditor
    - adjusted pagination to always show 3 pages (1,2,3 - 2,3,4 and so on)
    - adjusted forloop.counter in students_table.html (as per teacher request)

--- config/urls.py
+++ config/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from core.views import index
 
 from django.contrib import admin
@@ -29,4 +32,8 @@ urlpatterns = [
     path('teachers/', include('teachers.urls')),
     path('', index, name='index'),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/admin.py
@@ -0,0 +1,22 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.db import models
+
+
+class FlatPageCustomAdmin(FlatPageAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorUploadingWidget}
+    }
+
+    list_display = [
+        'url',
+        'title',
+        'template_name',
+    ]
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageCustomAdmin)
